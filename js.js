529ad6c4928f8fc1408a36c20228821a1adffaac
Hollie <hollie@marcloudconsulting.com>
2023-05-05 12:35:37 +0100

    modified:   js.js

--- js.js
+++ js.js
@@ -16,7 +16,7 @@ function setCookie(cName, cValue, expDays) {
     let date = new Date();
     date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
-    document.cookie = cName + "=" + cValue + "; " + expires + '; path=/' + '; domain=' + location.hostname + '; secure';
+    document.cookie = cName + "=" + cValue + "; " + expires + '; path=/' + '; domain=.' + location.hostname + '; secure';
 }
 
 function getCookie(cname) {
